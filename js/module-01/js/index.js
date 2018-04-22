48f3aa4e5a2d7c53cd0272754d7e8ab8e33fe9a9
Anatoly Korsun <korsoon@i.ua>
2018-04-23 01:53:32 +0300

    adds module-01 js

---
+++ js/module-01/js/index.js
@@ -0,0 +1,64 @@
+const GROUP_SHARM = " Sharm ";
+const GROUP_HURGADA = " Hurgada ";
+const GROUP_TABA = " Taba ";
+
+const PLACES_SHARM_TOUR = 15;
+const PLACES_HURGADA_TOUR = 25;
+const PLACES_TABA_TOUR = 6;
+
+let userInput = prompt(
+  "Сколько человек будет отдыхать с Вами",
+  "от 6 до 25"
+);
+const touristPlacaes = Number(userInput);
+
+console.log(touristPlacaes);
+
+const isValidInput =
+  userInput !== null &&
+  !Number.isNaN(touristPlacaes) &&
+  userInput > 0 &&
+  Number.isInteger(touristPlacaes);
+
+console.log(isValidInput);
+
+if (isValidInput) {
+  let leftPlacesTabaTour;
+  let leftPlacesSharmTour;
+  let leftPlacesHurgadaTour;
+
+  if (touristPlacaes <= PLACES_TABA_TOUR) {
+    if (
+      window.confirm(
+        `Есть места в группе ${GROUP_TABA}, ви согласны быть в этой группе?`
+      )
+    ) {
+      leftPlacesTabaTour = PLACES_TABA_TOUR - touristPlacaes;
+      window.alert(
+        `Приятного путешествия в группе ${GROUP_TABA}, остаток мест по туру ${leftPlacesTabaTour}`
+      );
+    } else window.alert(`Нам очень жаль приходите еще.`);
+  } else if (touristPlacaes <= PLACES_SHARM_TOUR) {
+    if (
+      window.confirm(
+        `Есть места в группе ${GROUP_SHARM}, ви согласны быть в этой группе?`
+      )
+    ) {
+      leftPlacesSharmTour = PLACES_SHARM_TOUR - touristPlacaes;
+      window.alert(
+        `Приятного путешествия в группе ${GROUP_SHARM}, остаток мест по туру ${leftPlacesSharmTour}`
+      );
+    } else window.alert(`Нам очень жаль приходите еще.`);
+  } else if (touristPlacaes <= PLACES_HURGADA_TOUR) {
+    if (
+      window.confirm(
+        `Есть места в группе ${GROUP_HURGADA}, ви согласны быть в этой группе?`
+      )
+    ) {
+      leftPlacesHurgadaTour = PLACES_HURGADA_TOUR - touristPlacaes;
+      window.alert(
+        `Приятного путешествия в группе ${GROUP_HURGADA}, остаток мест по туру ${leftPlacesHurgadaTour}`
+      );
+    } else window.alert(`Нам очень жаль приходите еще.`);
+  } else window.alert(`Извените мест ну вообще нет`);
+} else window.alert("Ошибка ввода");
