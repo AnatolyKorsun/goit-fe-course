3e4088cabdd8b0221c368ea84df40f1ce4304697
Anatoly Korsun <korsoon@i.ua>
2018-04-30 14:09:41 +0300

    add changes module-02 js

--- js/module-02/js/level.js
+++ js/module-02/js/level.js
@@ -2,15 +2,24 @@ const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
 let max = 3;
 let userInput;
 
+for (let i = 0; i < max; i += 1) {
 
-for (let i = 0; i < max; i += 1){
-    
     userInput = prompt("Введіть пароль");
-        while (userInput !== null){
-    
-    if (passwords.includes(userInput)){
+
+    if (userInput === null) {
+        break;
+    }
+
+    if (passwords.includes(userInput)) {
         alert("Ласкаво просимо")
         break;
     }
-    alert(`Не вірний пароль, у вас залишилось ${max - 1 - i} спроб.`);
-}}
+
+    else if (max - i - 1 !== 0) {
+        alert(`Не вірний пароль, у вас залишилось ${max - 1 - i} спроб.`);
+    }
+
+    else if (i < max) {
+        alert('Спроби закінчилися. Акаунт заблоковано.');
+    }
+}
