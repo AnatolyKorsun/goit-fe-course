d8336917d1667b1517a6ecfb8030214284dbb232
Anatoly Korsun <korsoon@i.ua>
2018-05-07 03:56:18 +0300

    adds js module-03

---
+++ js/module-03/js/index.js
@@ -0,0 +1,38 @@
+"use atrict";
+
+const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
+
+const checkIfLoginExist = function(login, logins) {
+  for (const value of logins) {
+    let inExist;
+    if (value === login) {
+      inExist = true;
+      return inExist;
+    }
+  }
+};
+
+const checkLoginValidity = function(login) {
+  let isValid;
+  if (login.length < 4 || login.length > 15) {
+    isValid = false;
+  }
+  return isValid;
+};
+
+const addLogin = function(login, logins) {
+  if (checkLoginValidity(login) === false) {
+    console.log("Ошибка! Логин должен быть от 4 до 16 символов");
+    return;
+  }
+  if (checkIfLoginExist(login, logins) === true) {
+    console.log("Такой логин уже используется!");
+    return;
+  } else {
+    logins.push(login);
+    console.log("Логин успешно добавлен");
+  }
+};
+addLogin("Mango", logins);
+
+
