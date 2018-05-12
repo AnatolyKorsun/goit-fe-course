5ab7ddf8cc393e01cf5989efea579443fb6b56ff
Anatoly Korsun <korsoon@i.ua>
2018-05-12 18:23:56 +0300

    adds js module-04

--- js/module-02/js/level.js
+++ js/module-02/js/level.js
@@ -1,25 +1,31 @@
-const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
-let max = 3;
-let userInput;
+// const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
+// let max = 3;
+// let userInput;
 
-for (let i = 0; i < max; i += 1) {
+// for (let i = 0; i < max; i += 1) {
 
-    userInput = prompt("Введіть пароль");
+//     userInput = prompt("Введіть пароль");
 
-    if (userInput === null) {
-        break;
-    }
+//     if (userInput === null) {
+//         break;
+//     }
 
-    if (passwords.includes(userInput)) {
-        alert("Ласкаво просимо")
-        break;
-    }
+//     if (passwords.includes(userInput)) {
+//         alert("Ласкаво просимо")
+//         break;
+//     }
 
-    else if (max - i - 1 !== 0) {
-        alert(`Не вірний пароль, у вас залишилось ${max - 1 - i} спроб.`);
-    }
+//     else if (max - i - 1 !== 0) {
+//         alert(`Не вірний пароль, у вас залишилось ${max - 1 - i} спроб.`);
+//     }
 
-    else if (i < max) {
-        alert('Спроби закінчилися. Акаунт заблоковано.');
-    }
+//     else if (i < max) {
+//         alert('Спроби закінчилися. Акаунт заблоковано.');
+//     }
+// }
+
+
+const hotel = {
+    userName : 'Poly',
 }
+console.log(hotel.userName);
