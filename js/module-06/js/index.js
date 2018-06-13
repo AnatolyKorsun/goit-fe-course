731bbc99b97f5fdf621fb96bee0b7239be797c46
Anatoly Korsun <korsoon@i.ua>
2018-06-13 04:21:58 +0300

    adds changes js module-06

--- js/module-06/js/index.js
+++ js/module-06/js/index.js
@@ -6,6 +6,58 @@ class Hamburger {
         this.stuffing = obj.stuffing;
         this.TOPPING_SPICE = 0;
         this.TOPPING_SAUSE = 0;
+
+        this.getSize = function () {
+            return this.size
+        };
+
+        this.getStuffing = function () {
+            return this.stuffing
+        };
+
+        this.addTopping = function (topping) {
+            if (Hamburger[topping] === topping && this[topping] !== topping) {
+                this[topping] = topping;
+            };
+        };
+
+        this.removeTopping = function (topping) {
+            if (Hamburger[topping] === topping && this[topping] === topping) {
+                this[topping] = 0;
+            };
+        };
+
+        this.removeTopping = function (topping) {
+            if (Hamburger[topping] === topping && this[topping] === topping) {
+                this[topping] = 0;
+            };
+        };
+
+        this.getToppings = function () {
+            return Hamburger.TOPPINGS
+        };
+
+        this.calculatePrice = function () {
+            let totalPriceTopping = 0;
+            for (const key in Hamburger.TOPPINGS) {
+                if (Hamburger[key] === this[key]) {
+                    totalPriceTopping += Hamburger.TOPPINGS[key].price;
+                };
+            };
+            const totalPrice = Hamburger.SIZES[this.size].price + Hamburger.STUFFINGS[this.stuffing].price + totalPriceTopping;
+            return totalPrice
+        };
+
+        this.calculateCalories = function () {
+            let totalCaloriesTopping = 0;
+            for (const key in Hamburger.TOPPINGS) {
+                if (Hamburger[key] === this[key]) {
+                    totalCaloriesTopping += Hamburger.TOPPINGS[key].calories;
+                };
+            };
+            const totalCalories = Hamburger.SIZES[this.size].calories + Hamburger.STUFFINGS[this.stuffing].calories + totalCaloriesTopping;
+            return totalCalories
+        };
     };
 };
 
@@ -37,75 +89,30 @@ Hamburger.TOPPINGS = {
 
 const hamburger = new Hamburger({ size: Hamburger.SIZE_SMALL, stuffing: Hamburger.STUFFING_MEAT });
 
-Hamburger.prototype.getSize = function () {
-    return this.size
-};
-
-Hamburger.prototype.getStuffing = function () {
-    return this.stuffing
-};
-
-Hamburger.prototype.addTopping = function (topping) {
-    if (Hamburger[topping] === topping && this[topping] !== topping) {
-        this[topping] = topping;
-    };
-};
-
-Hamburger.prototype.removeTopping = function (topping) {
-    if (Hamburger[topping] === topping && this[topping] === topping) {
-        this[topping] = 0;
-    };
-};
-
-Hamburger.prototype.getToppings = function () {
-    return Hamburger.TOPPINGS
-};
-
-Hamburger.prototype.calculatePrice = function () {
-    let totalPriceTopping = 0;
-    for (const key in Hamburger.TOPPINGS) {
-        if (Hamburger[key] === this[key]) {
-            totalPriceTopping += Hamburger.TOPPINGS[key].price;
-        };
-    };
-    const totalPrice = Hamburger.SIZES[this.size].price + Hamburger.STUFFINGS[this.stuffing].price + totalPriceTopping;
-    return totalPrice
-};
-
-Hamburger.prototype.calculateCalories = function () {
-    let totalCaloriesTopping = 0;
-    for (const key in Hamburger.TOPPINGS) {
-        if (Hamburger[key] === this[key]) {
-            totalCaloriesTopping += Hamburger.TOPPINGS[key].calories;
-        };
-    };
-    const totalCalories = Hamburger.SIZES[this.size].calories + Hamburger.STUFFINGS[this.stuffing].calories + totalCaloriesTopping;
-    return totalCalories
-};
 //  * Узнать размер гамбургера
-// console.log('hamburger size:',hamburger.getSize());
+console.log('hamburger size:', hamburger.getSize());
 
 //  * Узнать начинку гамбургера
-//  console.log('stuffing:', hamburger.getStuffing());
+console.log('stuffing:', hamburger.getStuffing());
 
 //  * Добавить topping к гамбургеру. Можно добавить несколько topping, при условии, что они разные.
-// hamburger.addTopping('TOPPING_SPICE');
-// hamburger.addTopping('TOPPING_SAUSE');
-// console.log('topping:', hamburger.TOPPING_SAUSE);
-// console.log('topping:', hamburger.TOPPING_SPICE);
+hamburger.addTopping('TOPPING_SPICE');
+hamburger.addTopping('TOPPING_SAUSE');
+console.log('topping:', hamburger.TOPPING_SAUSE);
+console.log('topping:', hamburger.TOPPING_SPICE);
 
 
 // * Убрать topping, при условии, что она ранее была добавлена
-//  hamburger.removeTopping('TOPPING_SPICE');
-//  hamburger.removeTopping('TOPPING_SAUSE');
-//  console.log(hamburger.TOPPING_SPICE);
-//  console.log(hamburger.TOPPING_SAUSE);
+hamburger.removeTopping('TOPPING_SPICE');
+hamburger.removeTopping('TOPPING_SAUSE');
+console.log(hamburger.TOPPING_SPICE);
+console.log(hamburger.TOPPING_SAUSE);
 
 //  * Получить список toppings
-//  console.log(hamburger.getToppings());
+console.log(hamburger.getToppings());
 
 //  * Узнать цену гамбургера
-// console.log('total price;', hamburger.calculatePrice());
+console.log('total price;', hamburger.calculatePrice());
 
 //  * Узнать калорийность
-// console.log('total calories', hamburger.calculateCalories());
+console.log('total calories', hamburger.calculateCalories());
